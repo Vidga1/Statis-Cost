e9b54da2fd43ab7ab15cbabc746da0e8c4e0d991
Vladislav <vidga471@gmail.com>
2023-12-23 11:44:32 +0300

    set up project

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:storybook/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'react', 'react-refresh'],
+  rules: {
+    'import/prefer-default-export': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'jest/valid-title': 'off',
+    'no-console': 'off',
+    'no-alert': 'off',
+    'no-restricted-globals': 'off',
+    'no-plusplus': 'off',
+    'import/no-unresolved': 'off',
+    'import/extensions': 'off',
+    'no-restricted-syntax': ['off', 'ForOfStatement'],
+  },
+};
---
+++ jest.config.ts
@@ -0,0 +1,36 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  coverageThreshold: {
+    global: {
+      branches: 60,
+      functions: 60,
+      lines: 60,
+      statements: -20,
+    },
+  },
+  moduleFileExtensions: [
+    'js',
+    'mjs',
+    'cjs',
+    'jsx',
+    'ts',
+    'tsx',
+    'json',
+    'node',
+  ],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css|scss|less)$': '<rootDir>/__mocks__/styleMock.js',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testEnvironment: 'jsdom',
+  verbose: true,
+};
+
+export default config;
